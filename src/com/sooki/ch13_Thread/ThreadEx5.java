38d8fe8b712ecbc56bad0aa94c1bd1e17c9dda51
Jisu Lee <jennieee267@gmail.com>
2021-04-26 00:13:41 +0900

    Thread 예제

---
+++ src/com/sooki/ch13_Thread/ThreadEx5.java
@@ -0,0 +1,31 @@
+package com.sooki.ch13_Thread;
+
+/**
+ * 멀티쓰레드일 때 소요시간
+ */
+public class ThreadEx5 {
+    static long startTime = 0;
+
+    public static void main(String[] args) {
+        ThreadEx5_1 th1 = new ThreadEx5_1();
+        th1.start();
+        startTime = System.currentTimeMillis();
+
+        for (int i = 0; i < 500; i++)
+            System.out.printf("%s", new String("-"));
+
+        System.out.println("소요시간1 : " + (System.currentTimeMillis() - ThreadEx5.startTime));
+
+
+    }
+}
+
+class ThreadEx5_1 extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i < 500; i++)
+            System.out.printf("%s", new String("|"));
+
+        System.out.println("소요시간2 : " + (System.currentTimeMillis() - ThreadEx5.startTime));
+    }
+}
